ad35254579e508db34fa0899067b2c464f7a2045
orville94bailey <orville94bailey@gmail.com>
2019-12-08 11:35:05 -0500

    using wmata api to pull train info

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("hello world")
+import os
+import json
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+# Request headers
+        'api_key': os.environ['wmatatoken'],
+}
+
+params = urllib.parse.urlencode({
+})
+
+try:
+        conn = http.client.HTTPSConnection('api.wmata.com')
+        conn.request("GET", "/StationPrediction.svc/json/GetPrediction/F03?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        result = json.loads(data)
+        conn.close()
+except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+for item in result["Trains"]:
+        if item["Line"] == 'YL' and item["Destination"] == 'Hntingtn':
+                print(item)
+input()
